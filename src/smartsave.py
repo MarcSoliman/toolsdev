d6b4d19e0aa3ff2c301da6f209d7e7aeb72ff393
Marc S <marc.soliman@outlook.com>
2020-10-11 15:48:35 -0500

    Added Filename Fields

--- src/smartsave.py
+++ src/smartsave.py
@@ -32,10 +32,25 @@ class SmartSaveUI(QtWidgets.QDialog):
         self.title_lbl = QtWidgets.QLabel("Smart Save")
         self.title_lbl.setStyleSheet("font: bold 20px")
         self.folder_lay = self._create_folder_ui()
+        self.filename_lay = self._create_filename_ui()
         self.main_lay = QtWidgets.QVBoxLayout()
         self.main_lay.addWidget(self.title_lbl)
         self.main_lay.addLayout(self.folder_lay)
+        self.main_lay.addLayout(self.filename_lay)
         self.setLayout(self.main_lay)
+
+    def _create_filename_ui(self):
+        self.descriptor_header_lbl = QtWidgets.QLabel("Descriptor")
+        self.descriptor_header_lbl.setStyleSheet("font: bold")
+        self.task_header_lbl = QtWidgets.QLabel("Task")
+        self.task_header_lbl.setStyleSheet("font: bold")
+        self.ver_header_lbl = QtWidgets.QLabel("Version")
+        self.ver_header_lbl.setStyleSheet("font: bold")
+        layout = QtWidgets.QGridLayout()
+        layout.addWidget(self.descriptor_header_lbl, 0, 0)
+        layout.addWidget(self.task_header_lbl, 0, 2)
+        layout.addWidget(self.ver_header_lbl, 0, 4)
+        return layout
 
     def _create_folder_ui(self):
         default_folder = Path(cmds.workspace(rootDirectory=True, query=True))
@@ -50,6 +65,7 @@ class SmartSaveUI(QtWidgets.QDialog):
 
 class SceneFile(object):
     """An abstract representation of a scene file."""
+
     def __init__(self, path=None):
         self.folder_path = Path()
         self.descriptor = 'main'
@@ -83,7 +99,6 @@ class SceneFile(object):
         self.ext = path.ext
         self.descriptor, self.task, ver = path.name.stripext().split("_")
         self.ver = int(ver.split("v")[-1])
-
 
     def save(self):
         """save the scene file.
